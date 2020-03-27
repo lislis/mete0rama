1e4c8f421f5f13ece0927f3d111700fb656bcd1e
lislis <mail@lislis.de>
2020-03-27 19:50:07 +0100

    feat: add settings view

--- mete0rama-orama/src/components/AppFooter.vue
+++ mete0rama-orama/src/components/AppFooter.vue
@@ -2,6 +2,7 @@
   <footer class="appfooter">
       <p>Made with &lt;3 by <a href="https://lislis.de">lislis</a> and <a href="https://franka.tech">franka</a>.
       <p>&lt;/&gt; and project documentation under MIT license available on <a href="https://github.com/lislis/mete0rama">GitHub</a>.</p>
+      <p>Icons by <a href="https://fontawesome.com/">FontAwesome</a>, <a href="https://www.theleagueofmoveabletype.com/chunk">Chunk font</a> by Meredith Mandel.</p>
   </footer>
 </template>
 
