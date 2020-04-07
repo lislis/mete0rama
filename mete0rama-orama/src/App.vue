502285ca245829ef21334e1dd0c1ece38ab4d49c
lislis <mail@lislis.de>
2020-04-07 21:17:01 +0200

    feat(nav): use nav icons

--- mete0rama-orama/src/App.vue
+++ mete0rama-orama/src/App.vue
@@ -1,13 +1,19 @@
 <template>
 <div id="app">
-  <h1 class="app-title">Mete0rama</h1>
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/settings">Settings</router-link>
+  <header class="header">
+    <h1 class="app-title">Mete0rama</h1>
+    <div class="nav">
+      <router-link to="/" title="Home">
+        <img svg-inline class="icon" src="./assets/icons/home-solid.svg" alt="home" />
+      </router-link>
+      <router-link to="/about" title="Settings">
+        <img svg-inline class="icon" src="./assets/icons/cog-solid.svg" alt="settings" />
+      </router-link>
     </div>
-    <router-view/>
-    <AppFooter/>
-  </div>
+  </header>
+  <router-view/>
+  <AppFooter/>
+</div>
 </template>
 
 <script>
@@ -31,30 +37,58 @@ export default {
   font-style: normal;
 }
 
+html {
+  font-size: 1rem;
+  @media (max-width: 560px) {
+    font-size: 0.7rem;
+  }
+}
+
 body {
   background-color: #e6e6e6;
 }
 
 #app {
-  max-width: 70rem;
+  max-width: 65rem;
   margin: auto;
   font-size: 1.6rem;
   color: #333;
   font-family: Chunk, serif;
+  @media (max-width: 65rem) {
+
+  }
 }
 
 .app-title {
   font-size: 4rem;
 }
 
-#nav {
+.header {
+  display: flex;
+  align-items: baseline;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  margin-bottom: 2rem;
+}
 
+.nav {
+  display: flex;
+  align-content: center;
+  justify-content: space-between;
+  margin-bottom: 4rem;
+  flex: 0 0 6rem;
+  .icon {
+    width: 2rem;
+    height: 2rem;
+    opacity: 0.8;
+  }
   a {
-    font-weight: bold;
-    color: #2c3e50;
-
+    display: block;
+    padding: 0.5em;
+    line-height: 1;
+    border-radius: 10px;
     &.router-link-exact-active {
-      color: #42b983;
+      background-color: #ccc;
     }
   }
 }
